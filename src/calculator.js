c27f0e85041310b3755234ab33c413af2be2a8be
gn59762 <narragopikrishna@johndeere.com>
2019-11-19 15:03:53 -0600

    refactor

--- src/calculator.js
+++ src/calculator.js
@@ -26,7 +26,7 @@ function getMonthlyPayment() {
 }
 function getMonthlyPaymentsToCompare() {
     return <div>
-        <h5 className="h5">',' separated monthly payments to compare</h5>
+        <h5 className="h5">enter ',' separated monthly payments to compare</h5>
         <input type="string" onChange={(e) => this.addPayment(e)}/>
     </div>;
 }
