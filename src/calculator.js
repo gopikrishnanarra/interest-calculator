28bbe5f361c10004a9908c15ef460a5e7e949252
gn59762 <narragopikrishna@johndeere.com>
2019-11-19 09:35:24 -0600

    add compare payments table

--- src/calculator.js
+++ src/calculator.js
@@ -1,5 +1,36 @@
 import React, {Component} from 'react';
 
+function getInterest() {
+    return <div>
+        <h5 className="h5">enter interest rate</h5>
+        <input type="number" onChange={(e) => this.addInterest(e)}/>
+    </div>;
+}
+function getAmount() {
+    return <div>
+        <h5 className="h5">enter remaining amount</h5>
+        <input type="number" onChange={(e) => this.addAmount(e)}/>
+    </div>;
+}
+function getCloseButton() {
+    return <div>
+        <button onClick={() => this.closeComponent()}>close</button>
+    </div>
+}
+
+function getMonthlyPayment() {
+    return <div>
+        <h5 className="h5">amount you want to pay monthly</h5>
+        <input type="number" onChange={(e) => this.addPayment(e)}/>
+    </div>;
+}
+function getMonthlyPaymentsToCompare() {
+    return <div>
+        <h5 className="h5">amount you want to pay monthly</h5>
+        <input type="string" onChange={(e) => this.addPayment(e)}/>
+    </div>;
+}
+
 export default class Calculator extends Component {
       constructor(props) {
           super(props);
@@ -7,9 +38,12 @@ export default class Calculator extends Component {
               amount: 0,
               interest: 0,
               paying: 0,
+              compare: false,
+              calculate: false,
               nextPayment: 0,
               payments: [],
-              interests: []
+              interests: [],
+              interestsToCompare: []
           }
       }
     addAmount(e) {
@@ -27,13 +61,35 @@ export default class Calculator extends Component {
             paying: e.target.value
         })
     }
+    compare() {
+        this.setState({
+            compare: true
+        })
+    }
+    calculateInterests() {
+        this.setState({
+            calculate: true
+        })
+    }
+    closeComponent() {
+        this.setState({
+            amount: 0,
+            interest: 0,
+            paying: 0,
+            compare: false,
+            calculate: false,
+            nextPayment: 0,
+            payments: [],
+            interests: [],
+            interestsToCompare: {}
+        })
+    }
     getInterestPerDay() {
         let payments = [];
         let interests = [];
         let nextPayment = 0;
         const interest1 = ((this.state.amount * this.state.interest) / 100) / 12;
-        for(let i = 0; i <= 100; i++ ) {
-            if (nextPayment >= 0) {
+        for(let i = 0; nextPayment >= 0; i++ ) {
                 if (i === 0) {
                     payments[i] = parseInt(this.state.amount);
                     interests[i] = interest1.toFixed(3);
@@ -45,11 +101,36 @@ export default class Calculator extends Component {
                     interests[i] = interest.toFixed(3);
                     nextPayment = interest + payments[i] - parseInt(this.state.paying)
                 }
-            }
         }
         this.setState({
             payments: payments,
             interests: interests
+        });
+    }
+
+    getInterestsToCompare() {
+          const payments = this.state.paying.split(',');
+        let interestsToCompare = [];
+
+        payments.forEach((payment) => {
+              let interests = [];
+              let nextPayment = 0;
+              for(let i = 0; nextPayment >= 0; i++ ) {
+                  if (i === 0) {
+                      const interest = ((this.state.amount * this.state.interest) / 100) / 12;
+                      interests[i] = interest.toFixed(3);
+                      nextPayment = ((parseInt(this.state.amount) + interest) - parseInt(payment))
+                  }
+                  if (i !== 0) {
+                      const interest = ((parseInt(nextPayment) * this.state.interest) / 100) / 12;
+                      interests[i] = interest.toFixed(3);
+                      nextPayment = interest + parseInt(nextPayment) - parseInt(payment)
+                  }
+              }
+            interestsToCompare[interests.length] = [payment, interests.reduce((a, b) => parseFloat(a) + parseFloat(b), 0).toFixed(2)];
+          });
+        this.setState({
+            interestsToCompare
         });
     }
 
@@ -64,44 +145,80 @@ render() {
                   </tr>)
           })
           }
+          function getComparePaymentsData() {
+              return this.state.interestsToCompare.map(interest => {
+                  return ( <tr className="td">
+                      <td className="td">{interest[0]}</td>
+                      <td className="td">{this.state.interestsToCompare.indexOf(interest)}</td>
+                      <td className="td">{interest[1]}</td>
+                  </tr>)
+          })
+          }
           function isButtonDisabled() {
               return !this.state.amount || !this.state.paying || !this.state.interest
           }
+
     return (
               <div>
+                  {!this.state.calculate && !this.state.compare &&
+                      <div>
+                          <button onClick={() => this.calculateInterests()}>calculate payments</button>
+                          <button onClick={() => this.compare()}>compare interests</button>
+                      </div>
+                  }
+                  {this.state.calculate &&
                   <div>
-                          <h5 className="h5">enter remaining amount</h5>
-                        <input type="number" onChange={(e) => this.addAmount(e)}/>
-                  </div>
-                  <div>
-                         <h5 className="h5">enter interest rate</h5>
-                        <input type="number" onChange={(e) => this.addInterest(e)}/>
-                  </div>
-                  <div>
-                     <h5 className="h5">amount you want to pay monthly</h5>
-                     <input type="number" onChange={(e) => this.addPayment(e)}/>
-                  </div>
-                  <button disabled={isButtonDisabled.call(this)} onClick={() => this.getInterestPerDay()}>get payments</button>
+                      {getAmount.call(this)}
+                      {getInterest.call(this)}
+                      {getMonthlyPayment.call(this)}
+                      <button disabled={isButtonDisabled.call(this)} onClick={() => this.getInterestPerDay()}>get
+                          payments
+                      </button>
+                      {getCloseButton.call(this)}
 
-                  {this.state.payments.length > 0 &&
-                  <div>
-                  <h2>Balance</h2>
-                  <h3>You will finish your financing in {this.state.payments.length} months at rate
-                      of {this.state.paying}/month</h3>
-                  <h3>Total interest paying {this.state.interests.reduce((a,b) => parseFloat(a) + parseFloat(b), 0).toFixed(2)}</h3>
-                  this table will show maximum of 100 payments
-                  <table className="table">
-                      <tbody>
-                      <tr className="td">
-                          <th className="th">payment months from today</th>
-                          <th className="th">balance remained</th>
-                          <th className="th">interest added</th>
-                      </tr>
-                      {getMonthsBalanceData.call(this)}
-                      </tbody>
-                  </table>
+                      {this.state.payments.length > 0 &&
+                      <div>
+                          <h2>Balance</h2>
+                          <h3>You will finish your financing in {this.state.payments.length} months at rate
+                              of {this.state.paying}/month</h3>
+                          <h3>Total interest
+                              paying {this.state.interests.reduce((a, b) => parseFloat(a) + parseFloat(b), 0).toFixed(2)}</h3>
+                          <table className="table">
+                              <tbody>
+                              <tr className="td">
+                                  <th className="th">payment months from today</th>
+                                  <th className="th">balance remained</th>
+                                  <th className="th">interest added</th>
+                              </tr>
+                              {getMonthsBalanceData.call(this)}
+                              </tbody>
+                          </table>
+                      </div>
+                      }
                   </div>
                   }
+                  {this.state.compare &&
+                  <div>
+                      {getAmount.call(this)}
+                      {getInterest.call(this)}
+                      {getMonthlyPaymentsToCompare.call(this)}
+                      <button disabled={isButtonDisabled.call(this)} onClick={() => this.getInterestsToCompare()}>
+                          compare
+                      </button>
+                      {getCloseButton.call(this)}
+                      <table className="table">
+                          <tbody>
+                          <tr className="td">
+                              <th className="th">monthly payment</th>
+                              <th className="th">total months</th>
+                              <th className="th">total interest paid</th>
+                          </tr>
+                          {getComparePaymentsData.call(this)}
+                          </tbody>
+                      </table>
+                  </div>
+                  }
+
               </div>
           )
 }
